ea268735859fc5a41e930ffea58c51898a066eb1
Isaac <isaacw1925@gmail.com>
2024-10-13 02:18:39 -0700

    child key prop fix

--- frontend/src/Whiteboard.tsx
+++ frontend/src/Whiteboard.tsx
@@ -190,6 +190,7 @@ export default function Whiteboard() {
                 }
                 {elements.filter(e => e.type === 'text').map(e => (
                     <Item
+                        key={e.id}
                         e={e}
                         onDrag={(_, d) => {
                             if (selectedId === e.id && menuPos) {
